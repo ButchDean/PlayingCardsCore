4d6c0fb45102562630020c34bcf549a08a7d90a2
Dean Butcher <butchdeanca@gmail.com>
2022-09-20 20:17:33 -0400

    Source from main PlayingCardsCPPThreadsafe project.

---
+++ includes/progbar.h
@@ -0,0 +1,12 @@
+//
+// Created by Dean Butcher on 2021-10-24.
+//
+
+#ifndef _PROGBAR_H_
+#define _PROGBAR_H_
+
+typedef unsigned int uint;
+
+void UpdateProgress(const uint pct, const uint size);
+
+#endif //_PROGBAR_H_
