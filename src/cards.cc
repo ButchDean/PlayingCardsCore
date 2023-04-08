13e7ab989d1f18008473f8b8ed9b1d449017b11c
Dean Butcher <butchdeanca@gmail.com>
2023-04-07 22:18:55 -0400

    Squashed commit of the following:
    commit 3e7527a6cb13b2b84c50ee7a2255b16617b7b34b
    Author: Dean Butcher <butchdeanca@gmail.com>
    Date:   Fri Apr 7 22:07:13 2023 -0400
    
        Added exception handling to card deck creation.
    
    commit ac4604a8502c703e6d5ac33107a4efca9df9d643
    Author: Dean Butcher <butchdeanca@gmail.com>
    Date:   Wed Apr 5 19:38:28 2023 -0400
    
        New card deck contructor complete sans exception handling.
    
    commit 5956ee9e530e70b606f809b7fcd15bc85df6371e
    Author: Dean Butcher <butchdeanca@gmail.com>
    Date:   Wed Apr 5 19:15:01 2023 -0400
    
        Added card cvalue retrieval helper.
    
    commit 0eb7e51887f38d28742ae5618624112111c5019d
    Author: Dean Butcher <butchdeanca@gmail.com>
    Date:   Sun Apr 2 21:17:12 2023 -0400
    
        Added "INVALID_CARD".
    
    commit 81c6f67294e0f43ad332aceb3a0a6f95addaeb5c
    Author: Dean Butcher <butchdeanca@gmail.com>
    Date:   Sun Apr 2 19:11:33 2023 -0400
    
        Cardrefs filter to help construct card deck.
    
    commit e8a2fc54bda91421fddb2f3eea86645eb8b3da80
    Author: Dean Butcher <butchdeanca@gmail.com>
    Date:   Sun Apr 2 03:41:56 2023 -0400
    
        Constructing card string test.

--- src/cards.cc
+++ src/cards.cc
@@ -7,8 +7,7 @@
 
 namespace cards
 {
-	void CCardDeck::Init()
-	{
+	void CCardDeck::Init() {
 		std::puts("Initializing default sorted deck...");
 
 		deck.clear();
@@ -26,8 +25,194 @@ namespace cards
 		}
 	}
 
-	int CCardDeck::_GetRandomCardPos()
-	{
+	CardRefs CCardDeck::_CardRefFilter(unsigned int cvVal) const {
+		switch(cvVal) {
+			case 0:
+				return CardRefs::SPADES_A;
+			case 1:
+				return CardRefs::SPADES2;
+			case 2:
+				return CardRefs::SPADES3;
+			case 3:
+				return CardRefs::SPADES4;
+			case 4:
+				return CardRefs::SPADES5;
+			case 5:
+				return CardRefs::SPADES6;
+			case 6:
+				return CardRefs::SPADES7;
+			case 7:
+				return CardRefs::SPADES8;
+			case 8:
+				return CardRefs::SPADES9;
+			case 9:
+				return CardRefs::SPADES10;
+			case 10:
+				return CardRefs::SPADES_J;
+			case 11:
+				return CardRefs::SPADES_Q;
+			case 12:
+				return CardRefs::SPADES_K;
+			case 13:
+				return CardRefs::CLUBS_A;
+			case 14:
+				return CardRefs::CLUBS2;
+			case 15:
+				return CardRefs::CLUBS3;
+			case 16:
+				return CardRefs::CLUBS4;
+			case 17:
+				return CardRefs::CLUBS5;
+			case 18:
+				return CardRefs::CLUBS6;
+			case 19:
+				return CardRefs::CLUBS7;
+			case 20:
+				return CardRefs::CLUBS8;
+			case 21:
+				return CardRefs::CLUBS9;
+			case 22:
+				return CardRefs::CLUBS10;
+			case 23:
+				return CardRefs::CLUBS_J;
+			case 24:
+				return CardRefs::CLUBS_Q;
+			case 25:
+				return CardRefs::CLUBS_K;
+			case 26:
+				return CardRefs::HEARTS_A;
+			case 27:
+				return CardRefs::HEARTS2;
+			case 28:
+				return CardRefs::HEARTS3;
+			case 29:
+				return CardRefs::HEARTS4;
+			case 30:
+				return CardRefs::HEARTS5;
+			case 31:
+				return CardRefs::HEARTS6;
+			case 32:
+				return CardRefs::HEARTS7;
+			case 33:
+				return CardRefs::HEARTS8;
+			case 34:
+				return CardRefs::HEARTS9;
+			case 35:
+				return CardRefs::HEARTS10;
+			case 36:
+				return CardRefs::HEARTS_J;
+			case 37:
+				return CardRefs::HEARTS_Q;
+			case 38:
+				return CardRefs::HEARTS_K;
+			case 39:
+				return CardRefs::DIAMONDS_A;
+			case 40:
+				return CardRefs::DIAMONDS2;
+			case 41:
+				return CardRefs::DIAMONDS3;
+			case 42:
+				return CardRefs::DIAMONDS4;
+			case 43:
+				return CardRefs::DIAMONDS5;
+			case 44:
+				return CardRefs::DIAMONDS6;
+			case 45:
+				return CardRefs::DIAMONDS7;
+			case 46:
+				return CardRefs::DIAMONDS8;
+			case 47:
+				return CardRefs::DIAMONDS9;
+			case 48:
+				return CardRefs::DIAMONDS10;
+			case 49:
+				return CardRefs::DIAMONDS_J;
+			case 50:
+				return CardRefs::DIAMONDS_Q;
+			case 51:
+				return CardRefs::DIAMONDS_K;
+			default:
+				break;
+		}
+
+		return CardRefs::INVALID_CARD;
+	}
+
+	int CCardDeck::_CardRefFilterValue(const CardRefs crVal) {
+		switch(crVal) {
+			case CardRefs::SPADES_A:
+			case CardRefs::CLUBS_A:
+			case CardRefs::HEARTS_A:
+			case CardRefs::DIAMONDS_A:
+				return 1;
+			case CardRefs::SPADES2:
+			case CardRefs::CLUBS2:
+			case CardRefs::HEARTS2:
+			case CardRefs::DIAMONDS2:
+				return 2;
+			case CardRefs::SPADES3:
+			case CardRefs::CLUBS3:
+			case CardRefs::HEARTS3:
+			case CardRefs::DIAMONDS3:
+				return 3;
+			case CardRefs::SPADES4:
+			case CardRefs::CLUBS4:
+			case CardRefs::HEARTS4:
+			case CardRefs::DIAMONDS4:
+				return 4;
+			case CardRefs::SPADES5:
+			case CardRefs::CLUBS5:
+			case CardRefs::HEARTS5:
+			case CardRefs::DIAMONDS5:
+				return 5;
+			case CardRefs::SPADES6:
+			case CardRefs::CLUBS6:
+			case CardRefs::HEARTS6:
+			case CardRefs::DIAMONDS6:
+				return 6;
+			case CardRefs::SPADES7:
+			case CardRefs::CLUBS7:
+			case CardRefs::HEARTS7:
+			case CardRefs::DIAMONDS7:
+				return 7;
+			case CardRefs::SPADES8:
+			case CardRefs::CLUBS8:
+			case CardRefs::HEARTS8:
+			case CardRefs::DIAMONDS8:
+				return 8;
+			case CardRefs::SPADES9:
+			case CardRefs::CLUBS9:
+			case CardRefs::HEARTS9:
+			case CardRefs::DIAMONDS9:
+				return 9;
+			case CardRefs::SPADES10:
+			case CardRefs::CLUBS10:
+			case CardRefs::HEARTS10:
+			case CardRefs::DIAMONDS10:
+				return 10;
+			case CardRefs::SPADES_J:
+			case CardRefs::CLUBS_J:
+			case CardRefs::HEARTS_J:
+			case CardRefs::DIAMONDS_J:
+				return 11;
+			case CardRefs::SPADES_Q:
+			case CardRefs::CLUBS_Q:
+			case CardRefs::HEARTS_Q:
+			case CardRefs::DIAMONDS_Q:
+				return 12;
+			case CardRefs::SPADES_K:
+			case CardRefs::CLUBS_K:
+			case CardRefs::HEARTS_K:
+			case CardRefs::DIAMONDS_K:
+				return 13;
+			default:
+				break;
+		}
+
+		return -1;
+	}
+
+	int CCardDeck::_GetRandomCardPos() {
 		std::chrono::system_clock::time_point tp = std::chrono::system_clock::now();
 		std::chrono::system_clock::duration periods = tp.time_since_epoch();
 
@@ -40,8 +225,7 @@ namespace cards
 		return deckPos();
 	}
 
-	void CCardDeck::Shuffle()
-	{
+	void CCardDeck::Shuffle() {
 		// Do we need to shuffle?
 		auto deckSize = deck.size();
 
@@ -81,8 +265,7 @@ namespace cards
 		}
 	}
 
-	CardRefs CCardDeck::Draw()
-	{
+	CardRefs CCardDeck::Draw() {
 		if(deck.empty())
 		{
 			std::puts("Attempted to deal but deck is empty.");
@@ -98,8 +281,7 @@ namespace cards
 		return currentCard;
 	}
 
-	std::string CCardDeck::CardToStr(const CardRefs card) const
-	{
+	std::string CCardDeck::CardToStr(const CardRefs card) const {
 		auto cardSearch = std::move(cardDetail.find(card));
 
 		if(cardSearch == cardDetail.end())
@@ -108,8 +290,7 @@ namespace cards
 		return cardSearch->second.cardStr;
 	}
 
-	int CCardDeck::CardValue(const CardRefs card) const
-	{
+	int CCardDeck::CardValue(const CardRefs card) const {
 		auto cardSearch = std::move(cardDetail.find(card));
 
 		if(cardSearch == cardDetail.end())
